b22ba1c2539df852334b1bbf0a024e5773e6713a
sg-2604 <2k22.cse.2213375@gmail.com>
2025-06-19 17:41:41 +0530

    Initial commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order Home-Made Food Here</h2>
+            <p>Enjoy delicious, hygienic, and affordable meals, all with a personal touch — because nothing tastes better than home.</p>
+            <button>View Menu</button>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header
